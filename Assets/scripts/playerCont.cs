c7882cd59b5e962222b6ef907637127bd82fca8f
zrubens <thedogtiredcat@gmail.com>
2017-07-20 00:19:43 -0500

    Z Update 7/20
    Added basic movement, colliders, rigidbodies, and masks for audience sprites.

---
+++ Assets/scripts/playerCont.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCont : MonoBehaviour {
+
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement();
+	}
+
+    //movement method
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector2.up * speed * Time.deltaTime);
+        }
+    }
+}
